1c5e43b3c0b385e30ea93e9a69a5badfc89430dd
Angela DiSalvo <angela.disalvo@gmail.com>
2018-05-11 20:43:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var empty = []
+  for (let i=0; i < musicians.length; i++)
+  {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return empty
+}
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+    facts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return facts
+}
